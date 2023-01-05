11fe8750174e5de41659c06332d902ce1cb7de0e
ctang415 <carrietang117@gmail.com>
2023-01-05 14:07:41 -0500

    Added more unit tests for caesarcipher function

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,7 +1,16 @@
 const Caesarcipher = require('./caesarCipher')
 
-
-test('takes a string and returns each character shifted one', () => {
+test('checks for cipher shift of 10', () => {
     const cipher = new Caesarcipher()
-    expect(cipher.createCipher(`This is a caesar cipher! Isn\'t it?`, 5)).toMatch(/Ymnx nx f hfjxfw hnumjw! Nxs'y ny?/)
+    expect(cipher.createCipher('shift', 10)).toMatch(/crspd/)
+})
+
+test('checks for uppercase', () => {
+    const cipher = new Caesarcipher()
+    expect(cipher.createCipher('THIS IS UPPERCASE', 5)).toMatch(/YMNX NX ZUUJWHFXJ/)
+})
+
+test('checks for punctuation', () => {
+    const cipher = new Caesarcipher()
+    expect(cipher.createCipher(`this is a caesar cipher! isn\'t it?`, 5)).toMatch(/ymnx nx f hfjxfw hnumjw! nxs'y ny?/)
 })
